b66fc28058c0649f606033521db1cebf2903d117
Heirian <shinkcc@gmail.com>
2017-08-17 11:54:54 -0300

    Add all methods to User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,54 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: [:show, :edit, :update, :delete]
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
+
+  def show
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome #{@user.name} to Gamarch!"
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:danger] = "User and all associated articles have been deleted"
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
