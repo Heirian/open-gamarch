47ae9f5b77fcdfb089b19b3c22d57fdce064c511
Heirian <shinkcc@gmail.com>
2017-08-17 14:15:50 -0300

    Add articles resources and sessions to routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
   get '/about', to: 'pages#about'
   get '/starting', to: 'pages#starting'
 
+  resources :recipes
   get '/signup', to: 'users#new'
   resources :users, except: [:new]
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
