462eb696d26432b081ae3a6fdcc26704686804b2
Heirian <shinkcc@gmail.com>
2017-08-17 12:08:34 -0300

    Add validates to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true, length: { minimum: 5, maximum: 5000 }
+  belongs_to :user
+  validates :user_id, presence: true
+  default_scope -> { order(updated_at: :desc) }
 end
