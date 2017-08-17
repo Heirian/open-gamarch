6bc6e548e4d73d0755b0f085bb03b7c7d92a85b8
Heirian <shinkcc@gmail.com>
2017-08-17 11:58:38 -0300

    Add all methods to Articles Controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,53 @@
 class ArticlesController < ApplicationController
-  
+  before_action :set_article, only: [:show, :edit,:update, :destroy]
+
+  def index
+    @articles = Article.paginate(page: params[:page], per_page: 5)
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+    if @article.save
+      flash[:success] = "Article was created successfully!"
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:success] = "Article was updated successfully!"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    flash[:success] = "Article was deleted successfully!"
+    redirect_to articles_path
+  end
+
+  private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:name, :description)
+    end
 end
